02628002cfde28e7a552e94c817659c1384acdfa
Miguel Angel JC <mjaimes@madbius.com>
2021-10-06 11:46:57 -0500

    vista permisos

--- screens/permissionsTest/index.js
+++ screens/permissionsTest/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, StyleSheet, Text, Button} from 'react-native';
 
 const PermissionScreen = ({navigation}) => {
   return (
-    <View>
-      <Text>PermissionScreen</Text>
+    <View style={styles.container}>
+      <Text>PRUEBA DE PERMISOS</Text>
+      <Button
+        title="CAMARA"
+        color="black"
+        onPress={() => console.log('CAMARA')}
+      />
+      <Button
+        title="LOCALIZACION"
+        color="red"
+        onPress={() => console.log('LOCALIZACION')}
+      />
+      <Button
+        title="DOCUMENTOS"
+        color="black"
+        onPress={() => console.log('DOCUMENTOS')}
+      />
+      <Button
+        title="SOLICITAR MULTIPLES PERMISOS"
+        color="red"
+        onPress={() => console.log('SOLICITAR MULTIPLES PERMISOS')}
+      />
+      <Button
+        title="VER PERMISOS CONCEDIDOS"
+        color="black"
+        onPress={() => console.log('VER PERMISOS CONCEDIDOS')}
+      />
       <Button
         title="Home"
-        color="blue"
+        color="red"
         onPress={() => navigation.navigate('Home')}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 20,
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
 
 export default PermissionScreen;
